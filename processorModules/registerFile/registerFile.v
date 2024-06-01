b551b33c276c138f589dc613da8ede819da945a4
SondosAFarrah <132385970+SondosAFarrah@users.noreply.github.com>
2024-06-01 23:47:48 +0300

    Add files via upload

--- processorModules/registerFile/registerFile.v
+++ processorModules/registerFile/registerFile.v
@@ -1,1 +1,43 @@
+module registerFile(clock, RA, RB, RW, sig_enable_write, BusW, BusA, BusB);
+    
+	input wire clock;
 
+	input wire sig_enable_write;
+
+	input wire [2:0] RA, RB, RW;
+    
+	output reg [15:0] BusA, BusB; 
+	
+	
+	input wire [15:0] BusW;
+
+	 reg [15:0] registers_array [0:7];
+	
+	always @(posedge clock) begin
+		if(!sig_enable_write) begin
+			BusA = registers_array[RA];
+			BusB = registers_array[RB];
+		end
+	end
+
+	always @(posedge sig_enable_write ) begin
+		
+		if ( RW != 3'b0 && sig_enable_write) begin
+			registers_array[RW] = BusW;	
+		end
+		
+	end
+
+	initial begin
+		// Initialize registers to be zeros
+		registers_array[0] <= 16'h0000;
+		registers_array[1] <= 16'h0000;
+		registers_array[2] <= 16'h0000;
+		registers_array[3] <= 16'h0000;
+		registers_array[4] <= 16'h0000;
+		registers_array[5] <= 16'h0000;
+		registers_array[6] <= 16'h0000;	
+		registers_array[7] <= 16'h0000;		
+
+	end
+endmodule						 
---
+++ processorModules/registerFile/registerFileTestBench.v
@@ -0,0 +1,72 @@
+module registerFileTestBench ();
+
+    // ----------------- CLOCK -----------------
+    
+    // clock generator wires/registers
+	wire clock;
+
+	// generates clock square wave with 10ns period
+	ClockGenerator clock_generator(clock);
+
+    // ----------------- Register File -----------------
+ 
+    // register file wires/registers
+    reg [2:0] RA, RB, RW;
+    reg sig_enable_write;
+    reg [15:0] BusW;
+    wire [15:0] BusA, BusB;	 
+	
+    
+    // register file ( array of 16 registers )
+    registerFile register_file(clock, RA, RB, RW, sig_enable_write, BusW, BusA, BusB);
+    
+    // ----------------- Simulation -----------------
+
+    initial begin
+
+        
+        // test the register file by writing and reading some values from it from different registers
+        // and checking if the values are correct
+
+        #0
+        RW <= 3'd1; // write to register 1
+        BusW <= 16'd8; // write value 8
+        sig_enable_write <= 1; // enable write
+
+        #10
+        RW <= 3'd2; // write to register 2
+        BusW <= 16'd16; // write value 16
+        sig_enable_write <= 1; // enable write
+
+        #10
+        RA <= 3'd1;
+        RB <= 3'd2;
+        sig_enable_write <= 0; // enable write
+
+        #10
+
+        // results of previouse cycle
+        $display("(%0t) > BusA=%0d, BusB=%0d", $time, BusA, BusB);
+
+
+        // test overwrite
+        RW <= 3'd2; // write to register 2
+        BusW <= 16'd32; // write value 32
+        sig_enable_write <= 1; // enable write
+
+        #10
+        $display("(%0t) > BusA=%0d, BusB=%0d", $time, BusA, BusB);
+
+        // test write without write signal
+        RW <= 5'd2; // write to register 2
+        BusW <= 16'd64; // write value 32
+        sig_enable_write <= 0; // disable write
+
+        #10
+        $display("(%0t) > BusA=%0d, BusB=%0d", $time, BusA, BusB);
+
+
+        #10 $finish;    // 100 cycle
+    end
+
+endmodule
