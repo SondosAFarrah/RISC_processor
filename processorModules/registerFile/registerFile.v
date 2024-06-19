653ed5ba5331522349f5ca2a138f09ec66169ec7
SondosAFarrah <132385970+SondosAFarrah@users.noreply.github.com>
2024-06-19 23:34:44 +0300

    Add files via upload

--- processorModules/registerFile/registerFile.v
+++ processorModules/registerFile/registerFile.v
@@ -1,29 +1,33 @@
-module registerFile(clock, RA, RB, RW, sig_enable_write, BusW, BusA, BusB);
+module registerFile(clock, RA, RB, RW, sig_enable_write1,sig_enable_write2, BusW1, BusW2, BusA, BusB,registers_array);
     
 	input wire clock;
 
-	input wire sig_enable_write;
+	input wire sig_enable_write1 , sig_enable_write2;
 
 	input wire [2:0] RA, RB, RW;
     
 	output reg [15:0] BusA, BusB; 
 	
 	
-	input wire [15:0] BusW;
+	input wire [15:0] BusW1,BusW2;
 
-	 reg [15:0] registers_array [0:7];
+	 output reg [15:0] registers_array [0:7];
 	
 	always @(posedge clock) begin
-		if(!sig_enable_write) begin
-			BusA = registers_array[RA];
+		if(sig_enable_write1==0 && sig_enable_write2==0) begin
 			BusB = registers_array[RB];
+			BusA = registers_array[RA];
+			
 		end
 	end
 
-	always @(posedge sig_enable_write ) begin
+	always @(posedge (sig_enable_write1 ||  sig_enable_write2) ) begin
 		
-		if ( RW != 3'b0 && sig_enable_write) begin
-			registers_array[RW] = BusW;	
+		if ( RW != 3'b0 && sig_enable_write1) begin
+			registers_array[RW] = BusW1;	
+		end
+		if ( RW != 3'b0 && sig_enable_write2) begin
+			registers_array[RW] = BusW2;	
 		end
 		
 	end
@@ -31,9 +35,9 @@ module registerFile(clock, RA, RB, RW, sig_enable_write, BusW, BusA, BusB);
 	initial begin
 		// Initialize registers to be zeros
 		registers_array[0] <= 16'h0000;
-		registers_array[1] <= 16'h0000;
-		registers_array[2] <= 16'h0000;
-		registers_array[3] <= 16'h0000;
+		registers_array[1] <= 16'h4444;
+		registers_array[2] <= 16'h0001;
+		registers_array[3] <= 16'hFFFF;
 		registers_array[4] <= 16'h0000;
 		registers_array[5] <= 16'h0000;
 		registers_array[6] <= 16'h0000;	
--- processorModules/registerFile/registerFileTestBench.v
+++ processorModules/registerFile/registerFileTestBench.v
@@ -12,13 +12,13 @@ module registerFileTestBench ();
  
     // register file wires/registers
     reg [2:0] RA, RB, RW;
-    reg sig_enable_write;
-    reg [15:0] BusW;
+    reg sig_enable_write1,sig_enable_write2;
+    reg [15:0] BusW1,BusW2;
     wire [15:0] BusA, BusB;	 
-	
+	reg [15:0] registers_array [0:7];
     
     // register file ( array of 16 registers )
-    registerFile register_file(clock, RA, RB, RW, sig_enable_write, BusW, BusA, BusB);
+    registerFile register_file(clock, RA, RB, RW, sig_enable_write1,sig_enable_write2, BusW1, BusW2, BusA, BusB,registers_array);
     
     // ----------------- Simulation -----------------
 
@@ -30,19 +30,22 @@ module registerFileTestBench ();
 
         #0
         RW <= 3'd1; // write to register 1
-        BusW <= 16'd8; // write value 8
-        sig_enable_write <= 1; // enable write
+        BusW1 <= 16'd8; // write value 8
+        sig_enable_write1 <= 1; // enable write
+		sig_enable_write2 <= 0;
 
         #10
         RW <= 3'd2; // write to register 2
-        BusW <= 16'd16; // write value 16
-        sig_enable_write <= 1; // enable write
-
+        BusW2 <= 16'd0; // write value 16
+        sig_enable_write2 <= 1; // enable write
+		sig_enable_write1 <= 0;
+		
         #10
-        RA <= 3'd1;
-        RB <= 3'd2;
-        sig_enable_write <= 0; // enable write
-
+        RA <= 3'd2;
+        RB <= 3'd1;
+        sig_enable_write1 <= 0; // disable write
+		sig_enable_write2 <= 0;
+		
         #10
 
         // results of previouse cycle
@@ -51,17 +54,19 @@ module registerFileTestBench ();
 
         // test overwrite
         RW <= 3'd2; // write to register 2
-        BusW <= 16'd32; // write value 32
-        sig_enable_write <= 1; // enable write
-
+        BusW1 <= 16'd32; // write value 32
+        sig_enable_write1 <= 1; // enable write
+		sig_enable_write2 <= 0;
+		
         #10
         $display("(%0t) > BusA=%0d, BusB=%0d", $time, BusA, BusB);
 
         // test write without write signal
         RW <= 5'd2; // write to register 2
-        BusW <= 16'd64; // write value 32
-        sig_enable_write <= 0; // disable write
-
+        BusW1 <= 16'd64; // write value 32
+        sig_enable_write1 <= 0; // disable write
+		sig_enable_write2 <= 0;
+		
         #10
         $display("(%0t) > BusA=%0d, BusB=%0d", $time, BusA, BusB);
 
